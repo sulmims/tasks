cbc49dd97f27582985c0933b7db2d8f8de4f5f2d
Michał Sulżycki <michal.sulzycki@sage.com>
2022-07-15 20:42:51 +0200

    First commit. Working code. Simplest version. No tests.

---
+++ figures-counter/src/main/java/LetterSource.java
@@ -0,0 +1,14 @@
+public class LetterSource{
+    private static final String LETTERS = "ABCDE";
+    private int letterNumber;
+
+    public LetterSource(){
+        letterNumber = 0;
+    }
+
+    public char getNextLetter(){
+        char letter = LETTERS.charAt(letterNumber);
+        letterNumber = letterNumber < LETTERS.length() - 1 ? letterNumber + 1 : 0;
+        return letter;
+    }
+}
